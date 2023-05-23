1df9501551387cc53ed9ba79e76764679766b926
nitishnmathur87 <mathur.nitish@gmail.com>
2023-05-23 14:24:45 -0400

    chore: localise app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t } = useTranslation();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{t('heading')}</h1>
+        <h1>{t('main.greeting', {name: 'Foo Bar'})}</h1>
+        <h2>{t('main.description')}</h2>
       </header>
     </div>
   );
